fd5d4bf81f21862b5241815d0d80818d0fbf7c76
Olena Rudenko <olena.rudenko@gmail.com>
2024-10-30 15:39:47 +0200

    2024 update cand skills

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
+import re
 from collections import Counter
 import matplotlib.pyplot as plt
 import plotly.express as px
@@ -14,11 +15,9 @@ header = st.container()
 data_and_selection = st.container()
 graphs = st.container()
 
-#change with css i have no idea how 
-#caching, not to do it many times 
-@st.cache
+@st.cache_data
 def get_data(filename):
-    data = pd.read_csv(filename)
+    data = pd.read_csv(filename).fillna('')
 
     return data 
 
@@ -26,48 +25,41 @@ def get_data(filename):
 with header: 
 
     st.title('Найпопулярніші навички в ІТ')
-    st.markdown('Ми проаналізували дані з профілів **72 тисяч кандидатів** та **90 тисяч вакансій** на Джині. \
-        Обирайте категорію нижче, щоб побачити ключові навички, які зназначають кандидати та про які пишуть у вакансіях.  \
-         \n -  Вибірка кандидатів — були активними за останні 30 днів.  \
-        \n - Вибірка вакансій: створені цього року та хоча б раз опубліковані. Поле навичок не порожнє.  \
+    st.markdown('Ми проаналізували дані з профілів **176 тисяч кандидатів**, які оновлювали профіль на Джині з початку 2024, та **понад 90 тисяч** опублікованих цього року вакансій.\
+        Обирайте категорію нижче, щоб побачити ключові навички, які кандидати вказують у профілях, а рекрутери вимагають у вакансіях.  \
         \n Підписуйтесь на [телеграм канал  Djinni.co](https://t.me/djinni_official)' )
-
+#candidates who activated profile this year, jobs published this year. підкатегорій немає 
 with data_and_selection: 
     st.subheader('Оберіть категорію')
-#loading cand data 
-    cand = get_data('skills_candidates.csv')
-    cand = cand.dropna()
-    cand.rename(columns = {'skills_cache':'all_skils'}, inplace=True)
-
-#loading jobs data 
-    jobs = get_data('skills_from_jobs.csv')
-    jobs = jobs.dropna()
-   # jobs['all_skils'] = jobs['primary_keyword'] + ',' + jobs['extra_keywords']
-    jobs['all_skils'] = jobs['extra_keywords']
-    jobs['all_skils'] = jobs['all_skils'].str.split(",").map(set).str.join("/")
-    jobs['all_skils'] = jobs['all_skils'].str.split("/").map(set).str.join("\n")
-    #jobs['all_skils'] = jobs['all_skils'].str.strip('\n')
-
+#loading data 
+    cand = get_data('candidate_activated_skills.csv')
+    jobs = get_data('jobs_with_label_published_2024.csv')
+    jobs.rename(columns={'extra_keywords': 'skills_cache'}, inplace=True)
+  
  #the dropdown. value = '' (defealt)
-    options = cand['primary_keyword'].unique()
+    options = cand['label'].unique()
     options = np.insert(options,0,'')
     options = np.sort(options)
     keyword = st.selectbox('Порожній вибір показує всі категорії', options = options, index =0) 
 
 
 # the function takes data and category and returns a list of skills in category in order of popularity 
+    exceptions = {"AI/ML", "CI/CD"}
     def one_list(data, keyword=''):
-        a = data
+        a = data.copy()
         if keyword: 
-            a = data[data['primary_keyword'] == keyword] 
-        skils = a['all_skils']
-        list = skils.tolist()
-        all_skils = []
-        for i in range(len(list)):
-            a = list[i]
-            a = a.split('\n')
-            all_skils.append(a)
-        flat_listt = [item for sublist in all_skils for item in sublist]
+            a = data[data['label']==keyword]
+            a = a.copy()
+        # Process the 'all_skills' column
+        a['all_skills'] = a.apply(lambda row: 
+                            [sub_skill.strip()
+                                for skill in set(re.split(r'[\n,]', row['skills_cache']))
+                                for sub_skill in (skill.split('/') if skill not in exceptions else [skill])],
+                            axis=1)
+
+        b = a['all_skills'].tolist()
+        flat_listt = [item for sublist in b for item in sublist]
+        flat_listt = [item for item in flat_listt if not (isinstance(item, float) and math.isnan(item))]
         flat_list =[]
         for skill in flat_listt: 
             skill = skill.strip()
@@ -77,10 +69,8 @@ with data_and_selection:
         skillshare = pd.DataFrame.from_dict(myDict, orient='index')
         skillshare['skill'] = skillshare.index
         skillshare.rename(columns = {0:'count'}, inplace=True)
-        skillshare = skillshare[['skill', 'count']]
-        skillshare = skillshare.reset_index(drop=True)
-        skillshare = skillshare.sort_values(by='count', ascending=False)
-        
+        skillshare = skillshare[['skill', 'count']].sort_values(by='count', ascending=False).reset_index(drop=True)
+        skillshare['share'] = skillshare['count']/len(a)
         return skillshare
 #two columns with graphs 
 with graphs: 
@@ -91,14 +81,23 @@ with graphs:
     if keyword == '':
         num_cand = cand.shape[0]
     else:
-        num_cand = (cand[cand['primary_keyword'] == keyword]).shape[0]
+        num_cand = (cand[cand['label'] == keyword]).shape[0]
 
     cand_col.markdown('Проаналізовано кандидатів: {}'.format(num_cand))
     
-    cand_skills = one_list(cand, keyword).head(10)
+    cand_skills = one_list(cand, keyword).head(15)
+
+    fig_cand = px.bar(
+        cand_skills, 
+        x="count", 
+        y="skill", 
+        orientation='h',
+        height=600,
+        category_orders={"skill": cand_skills["skill"].tolist()}) 
+    
+    fig_cand.update_yaxes(tickfont=dict(size=14, color='black'))  # Customize size and color
 
 
-    fig_cand = px.bar(cand_skills, x="count", y="skill", orientation='h')
     cand_col.write(fig_cand)
 
 #the second column
@@ -106,15 +105,26 @@ with graphs:
     jobs_col.subheader('Найпопулярніші навички у вакансіях')
     if keyword == '':
         num_jobs = jobs.shape[0]
+        skil_jobs = jobs[jobs['skills_cache']!=''].shape[0]
     else:
-        num_jobs = jobs[jobs['primary_keyword'] == keyword].shape[0]
+        num_jobs = jobs[jobs['label'] == keyword].shape[0]
+        skil_jobs = jobs[(jobs['label'] == keyword)&(jobs['skills_cache']!='')].shape[0]
 
 
-    jobs_col.markdown('Проаналізовано вакансій: {}'.format(num_jobs))
+    jobs_col.markdown('Проаналізовано вакансій: {}. Вакансій з зазначеними навичками: {}'.format(num_jobs, skil_jobs))
+
     
-    job_skills = one_list(jobs, keyword).head(10)
+    job_skills = one_list(jobs, keyword)
+    job_skills = job_skills[job_skills['skill']!=''].head(15)
 
-    fig_jobs = px.bar(job_skills, x="count", y="skill", orientation='h')
-   
+    fig_jobs = px.bar(
+        job_skills, 
+        x="count", 
+        y="skill", 
+        orientation='h',
+        height=600,
+        category_orders={"skill": job_skills["skill"].tolist()}) 
+
+    fig_jobs.update_yaxes(tickfont=dict(size=14, color='black'))  # Customize size and color
 
     jobs_col.write(fig_jobs)
